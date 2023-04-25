d7284df13d2e8a9aaa61477dedefe7e4128e7d3a
AD30191 <130198066+AD30191@users.noreply.github.com>
2023-04-25 12:10:17 -0700

    Update DeveloperCommandPrompt.py

--- src/DeveloperCommandPrompt.py
+++ src/DeveloperCommandPrompt.py
@@ -4,9 +4,12 @@ import os
 # Was initially going to do this along with the build process but had no idea how to return control to the script
 # after the prompt was opened.
 
-# So, just run this and then run the Build.py script inside the opened terminal.
+# So, just run this and then run the Build.py script inside the opened terminal or just open the terminal manually.
 
 # It should work for a fresh intstall of visual studio or the build tools.
+
+# P.S. to run the scripts you can either go to where you downloaded them and do .\scriptname, or put the full path including the script name into
+# the command prompt.
 
 def main():
     os.chdir("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Visual Studio 2022\\Visual Studio Tools\\VC")
