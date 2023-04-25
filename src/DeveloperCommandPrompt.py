a1325805a1eed189866afd5f482d3fce4aa803f9
AD30191 <130198066+AD30191@users.noreply.github.com>
2023-04-25 12:06:50 -0700

    Update DeveloperCommandPrompt.py

--- src/DeveloperCommandPrompt.py
+++ src/DeveloperCommandPrompt.py
@@ -1,5 +1,12 @@
 import os
 
+# A shortcut to the Developer Command Prompt for runnings msvc C++ tools on the command line
+# Was initially going to do this along with the build process but had no idea how to return control to the script
+# after the prompt was opened.
+
+# So, just run this and then run the Build.py script inside the opened terminal.
+
+# It should work for a fresh intstall of visual studio or the build tools.
 
 def main():
     os.chdir("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Visual Studio 2022\\Visual Studio Tools\\VC")
